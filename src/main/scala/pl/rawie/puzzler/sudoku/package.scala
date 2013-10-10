bc1d8cfd1c8cb2c742df25239069db91c4568e04
Radoslaw Wieckowski <radoslaw.wieckowski@gmail.com>
2013-10-10 16:35:34 +0200

    For comprehension for slicing board

--- src/main/scala/pl/rawie/puzzler/sudoku/package.scala
+++ src/main/scala/pl/rawie/puzzler/sudoku/package.scala
@@ -5,26 +5,21 @@ import scala.Predef._
 package object sudoku {
   private val numbers = (1 to 9).toSet
   private val offsets = for (x <- 0 to 2; y <- 0 to 2) yield (x, y)
-
-  private class SudokuSeq(s: Seq[Int]) {
-    def isValid: Boolean = numbers.equals(s.toSet)
-  }
-
-  private implicit def seqToSudokuSeq(s: Seq[Int]) = new SudokuSeq(s)
+  private type Slicer = (Int => Seq[Int])
 
   def check(board: Array[Array[Int]]): Boolean = {
     def row(i: Int): Seq[Int] = board(i)
 
     def col(i: Int): Seq[Int] = board.map(_(i))
 
-    def box(i: Int): Seq[Int] = {
+    def block(i: Int): Seq[Int] = {
       val (x, y) = ((i % 3) * 3, (i / 3) * 3)
       offsets.map { case (dx, dy) => board(y + dy)(x + dx) }
     }
 
-    (0 to 8)
-      .map { i => row(i).isValid && col(i).isValid && box(i).isValid }
-      .find(!_)
-      .isEmpty
+    val slices = for (f <- List[Slicer](row, col, block); i <- 0 to 8)
+      yield numbers equals(f(i) toSet)
+
+    !slices.contains(false)
   }
 }
